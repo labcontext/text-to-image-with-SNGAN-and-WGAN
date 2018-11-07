c3ec655b89681536672e73b54c75244d68eb91ad
WooramKang <omelisra@naver.com>
2018-11-07 13:04:24 +0900

    DCGAN of TEXT to IMAGE with lots of trial

--- run_files/dcgan_generate.py
+++ run_files/dcgan_generate.py
@@ -1,7 +1,7 @@
-import numpy as np
-from random import shuffle
 import os
 import sys
+import numpy as np
+from random import shuffle
 
 
 def main():
@@ -12,22 +12,22 @@ def main():
     sys.path.append(os.path.join(current_dir, '..'))
     current_dir = current_dir if current_dir is not '' else '.'
 
-    img_dir_path = current_dir + '/data/pokemon/img'
-    txt_dir_path = current_dir + '/data/pokemon/txt'
+    img_dir_path = '/home/rd/recognition_reaserch/IMAGE_CAPTION/DATASET/val2014'
+    txt_dir_path = '/home/rd/recognition_reaserch/IMAGE_CAPTION/DATASET/captions'
     model_dir_path = current_dir + '/models'
 
-    img_width = 32
-    img_height = 32
+    img_width = 64
+    img_height = 64
 
-    from keras_text_to_image.library.dcgan import DCGan
-    from keras_text_to_image.library.utility.image_utils import img_from_normalized_img
-    from keras_text_to_image.library.utility.img_cap_loader import load_normalized_img_and_its_text
+    from TXT2IMAGE.library.dcgan_v3 import DCGanV3
+    from TXT2IMAGE.library.utility.image_utils import img_from_normalized_img
+    from TXT2IMAGE.library.utility.img_cap_loader import load_normalized_img_and_its_text
 
     image_label_pairs = load_normalized_img_and_its_text(img_dir_path, txt_dir_path, img_width=img_width, img_height=img_height)
 
     shuffle(image_label_pairs)
 
-    gan = DCGan()
+    gan = DCGanV3()
     gan.load_model(model_dir_path)
 
     for i in range(10):
@@ -36,10 +36,10 @@ def main():
         text = image_label_pair[1]
 
         image = img_from_normalized_img(normalized_image)
-        image.save(current_dir + '/data/outputs/' + DCGan.model_name + '-generated-' + str(i) + '-0.png')
+        image.save(current_dir + '/data/outputs/' + DCGanV3.model_name + '-generated-' + str(i) + '-0.png')
         for j in range(3):
             generated_image = gan.generate_image_from_text(text)
-            generated_image.save(current_dir + '/data/outputs/' + DCGan.model_name + '-generated-' + str(i) + '-' + str(j) + '.png')
+            generated_image.save(current_dir + '/data/outputs/' + DCGanV3.model_name + '-generated-' + str(i) + '-' + str(j) + '.png')
 
 
 if __name__ == '__main__':
--- run_files/dcgan_train.py
+++ run_files/dcgan_train.py
@@ -1,41 +1,47 @@
-import sys
 import os
+import sys
 import numpy as np
 from random import shuffle
 
 
 def main():
     seed = 42
+
     np.random.seed(seed)
 
     current_dir = os.path.dirname(__file__)
     sys.path.append(os.path.join(current_dir, '..'))
     current_dir = current_dir if current_dir is not '' else '.'
 
-    img_dir_path = current_dir + '/data/pokemon/img'
-    txt_dir_path = current_dir + '/data/pokemon/txt'
+    #img_dir_path = current_dir + '/data/demoset/img'
+    img_dir_path = '/home/rd/recognition_reaserch/IMAGE_CAPTION/DATASET/val2014'
+    txt_dir_path = '/home/rd/recognition_reaserch/IMAGE_CAPTION/DATASET/captions'
     model_dir_path = current_dir + '/models'
 
-    img_width = 32
-    img_height = 32
+    img_width = 64
+    img_height = 64
+
+    stage_width = 128
+    stage_height = 128
+
     img_channels = 3
 
-    from keras_text_to_image.library.dcgan import DCGan
-    from keras_text_to_image.library.utility.img_cap_loader import load_normalized_img_and_its_text
+    from TXT2IMAGE.library.dcgan_v3 import DCGanV3
+    from TXT2IMAGE.library.utility.img_cap_loader import load_normalized_img_and_its_text
 
     image_label_pairs = load_normalized_img_and_its_text(img_dir_path, txt_dir_path, img_width=img_width, img_height=img_height)
-
     shuffle(image_label_pairs)
 
-    gan = DCGan()
+    gan = DCGanV3()
     gan.img_width = img_width
     gan.img_height = img_height
+
     gan.img_channels = img_channels
-    gan.random_input_dim = 200
+    gan.random_input_dim = 50
     gan.glove_source_dir_path = './very_large_data'
 
-    batch_size = 16
-    epochs = 1000
+    batch_size = 8
+    epochs = 50000
     gan.fit(model_dir_path=model_dir_path, image_label_pairs=image_label_pairs,
             snapshot_dir_path=current_dir + '/data/snapshots',
             snapshot_interval=100,
